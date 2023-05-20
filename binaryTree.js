0fb8409a39deea2eaa6471c22363733b57bef43b
abhi27122002 <abhi.wagh2712@gmail.com>
2023-05-20 12:58:50 +0530

    first commint js

--- binaryTree.js
+++ binaryTree.js
@@ -1,9 +1,7 @@
 // Node class
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
-// var window_height = window.innerHeight;
-// var window_width = window.innerWidth;
-// canvas.width = window_width;
+
 // canvas.height = window_height;
 canvas.style.background = "grey";
 
