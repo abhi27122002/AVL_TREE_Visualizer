c1d7e3c3c21c0c163faa37a0466ec369413ab4f6
abhi27122002 <abhi.wagh2712@gmail.com>
2023-05-25 21:59:28 +0530

    all commits

--- AVLTree.js
+++ AVLTree.js
@@ -1,7 +1,11 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
-// canvas.height = window_height;
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+
 canvas.style.background = "grey";
 var x = 200;
 var y = 100;
@@ -41,6 +45,24 @@ class Node
         context.fillStyle="black";      //backgroundcolor of node
     } 
 
+    drawByParent(context){
+        console.log(this.parentX + " parentX");
+        console.log(this.parenty + "Parent y");
+        context.beginPath();
+        context.strokeStyle = 'blue';
+        context.textAlign = 'center';
+        context.textBaseline='middle';
+        context.font='20px Arial';
+        context.lineWidth = 5;
+        context.arc(this.parentX-100,this.parenty+100,this.radius,0,Math.PI *2, false);
+        context.stroke();
+        context.fill();
+        context.fillStyle="cyan";   
+        console.log("heelo");    //color of node
+        context.fillText(this.data, this.parentX-100, this.parenty+100);
+        context.fillStyle="black";      //backgroundcolor of node
+    } 
+
 
     drawlineLeft(context){
         context.moveTo(this.parentX+25, this.parenty);
@@ -53,6 +75,14 @@ class Node
         context.lineTo(this.xpos+25, this.ypos);
         context.stroke();
     }
+
+    update(){
+        context.clearRect(0, 0,window_width,window_height);
+        this.draw(context);
+        this.xpos += 10;
+        this.ypos += 10;
+    }
+
 }
 let dummy = null;
 let root2 = null;
@@ -97,11 +127,29 @@ class AVLTree{
     rotateRight(root){
         let temp;
         temp = root.left;
+
+
+        temp.xpos = root.xpos;
+        temp.ypos = root.ypos;
+
+        temp.left.parentX = temp.xpos;
+        temp.left.parenty = temp.ypos;
+
+        root.xpos = root.left.xpos;
+        if(root.right) root.ypos = root.right.ypos;
+
         root.left = temp.right;
         temp.right = root;
 
+        
+
         root.height = 1 + Math.max(this.heights(root.left), this.heights(root.right));
         temp.height = 1 + Math.max(this.heights(root.left), this.heights(root.right));
+
+        context.clearRect(0, 0,window_width,window_height);
+        temp.draw(context);
+        root.draw(context);
+        temp.left.drawByParent(context);
 
         return temp;
     }
@@ -140,32 +188,38 @@ class AVLTree{
         return root;
     }
 
+
+    drawWithParent(data){
+        let curr;
+        
+        var h = 0;
+        var parentXpos = dummy.xpos;
+        var parentYpos = dummy.ypos; 
+        h = heights(root2,dummy,1);  
+        if(dummy.flag == 1){      
+            curr = new Node(parentXpos + x_width[h-1],parentYpos + y,25,"black",data);
+        }else{
+           
+            curr = new Node(parentXpos - x_width[h-1],parentYpos +y,25,"black",data);
+        }
+        curr.parentX = dummy.xpos;
+        curr.parenty = dummy.ypos;
+        if(dummy.flag == 1){ 
+            curr.draw(context);
+            curr.drawlineLeft(context);
+        }else if(dummy.flag==0){
+            curr.draw(context);
+            curr.drawlineRight(context);
+        }else{}
+        return curr;
+    }
    
     insert(root,data){
         let curr;
         var bf = 0;
         var h = 0;
-
         if(root == null){     
-            var parentXpos = dummy.xpos;
-            var parentYpos = dummy.ypos; 
-            h = heights(root2,dummy,1);  
-            if(dummy.flag == 1){      
-                curr = new Node(parentXpos + x_width[h-1],parentYpos + y,25,"black",data);
-            }else{
-               
-                curr = new Node(parentXpos - x_width[h-1],parentYpos +y,25,"black",data);
-            }
-            curr.parentX = dummy.xpos;
-            curr.parenty = dummy.ypos;
-            if(dummy.flag == 1){ 
-                curr.draw(context);
-                curr.drawlineLeft(context);
-            }else if(dummy.flag==0){
-                curr.draw(context);
-                curr.drawlineRight(context);
-            }else{}
-            return curr;
+           return this.drawWithParent(data);
         }
 
         if(data < root.data){
@@ -179,10 +233,11 @@ class AVLTree{
             if(bf >= 2){
                 if(data < root.left.data){
                     console.log("LL");
-                    //root = LL(root);
+                    //root = this.LL(root);
+                    
                 }else{
                     console.log("LR");
-                    //root = LR(root);
+                    root = LR(root);
                 }
             }
 
@@ -197,34 +252,43 @@ class AVLTree{
             if(bf <= -2){
                 if(data > root.right.data){
                    console.log("RR");
-                   root = RR(root);
+                   //root = RR(root);
                 }
             }else{
                    console.log("RL");
-                   root = RL(root);
+                   //root = RL(root);
             }
         }else{
             return null;
         }
-
+        console.log("After LL");
+        this.inoder(root);
+        console.log("After LL pre");
+        this.preorder(root);
         root.height = 1 + Math.max(this.heights(root.left),this.heights(root.right));
         return root;
     }
 
-}
-
-
+};
 
 
 var AVL = new AVLTree();
 let root = new Node(1500,100,25,"black",50);
-root2 = root;
+root = root;
 root.draw(context);
-// AVL.insert(root,10);
-AVL.insert(root,5);
-AVL.insert(root,4);
 
-console.log("inorder")
+// let updateNode = function() {
+//     requestAnimationFrame(updateNode);
+//     root.update();
+// }
+// updateNode();
+// AVL.insert(root,10);
+
+root = AVL.insert(root,5);
+root = AVL.insert(root,4);
+
+
+console.log("inorder");
 AVL.inoder(root);
 console.log("preorder");
 AVL.preorder(root);
